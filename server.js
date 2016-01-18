2ab52807872fca9f1f856e76b136e75404df5ed4
syossan27 <wisdom1027@gmail.com>
2016-01-19 05:36:34 +0900

    implement

--- server.js
+++ server.js
@@ -1,38 +1,52 @@
-var express = require('express');
-var curl = require('node-curl');
-var qs = require('querystring');
-var app = express();
+var express = require('express')
+var app = express()
+var Curl = require('node-libcurl').Curl
+var qs = require('querystring')
+require('dotenv').load()
 
 process.on('uncaughtException', function(err) {
-  console.log(err);
-});
+  console.log(err)
+})
+
+app.use('/static', express.static('dist/static'))
+
+app.set('views', __dirname + '/dist')
+app.set('view engine', 'jade')
 
 app.get('/', function(req, res){
   // get AccessToken
-  var CLIENT_ID = ""
-  var CLIENT_SECRET = ""
-  var CODE = req.params.code
+  var CLIENT_ID = process.env.CLIENT_ID
+  var CLIENT_SECRET = process.env.CLIENT_SECRET
+  var CODE = req.query.code
   var targetUrl = "https://github.com/login/oauth/access_token"
 
-  var opts = {
-    POST: 1,
-    POSTFIELDS: qs.stringify({
+  var curl = new Curl()
+  curl.setOpt('URL', targetUrl)
+  curl.setOpt('POST', 1)
+  curl.setOpt('POSTFIELDS', qs.stringify({
       code: CODE,
       client_id: CLIENT_ID,
       client_secret: CLIENT_SECRET
-    })
-  };
+  }))
 
-  curl(targetUrl, opts, function(err) {
-    // access_token=0123456789abcdef0123456789abcdef01234567&token_type=bearer
-    // が返ってくるはず
-    console.log(this.body);
-    var access_token = 
-    // ↑を画面に表示し、設定ファイルに書き込むようメッセージを表示する。
-  });
-});
+  curl.on('end', function(statusCode, body, headers) {
+    var parameters = body.split('&')
+    var access_token = (parameters[0].split('='))[1]
 
-app.set('port', (process.env.PORT || 5000));
+    this.close()
+
+    if (access_token === "bad_verification_code") {
+      res.redirect('https://github.com/login/oauth/authorize?client_id=1c32c0ef97c2f71096fa')
+    } else {
+      res.render('index', { access_token: access_token })
+    }
+  })
+
+  curl.on('error', curl.close.bind(curl))
+  curl.perform()
+})
+
+app.set('port', (process.env.PORT || 5000))
 var server = app.listen(app.get('port'), function () {
-    console.log('Node app is running on port', app.get('port'));
-});
+    console.log('Node app is running on port', app.get('port'))
+})
